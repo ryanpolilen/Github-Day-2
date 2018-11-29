3025865069c9db1a918cf5249eebf63f2f2c3615
alexandriabayas <45398974+alexandriabayas@users.noreply.github.com>
2018-11-29 12:08:05 -0500

    Update Day2.java
    Added method to convert arraylist to integer

--- Day2.java
+++ Day2.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
 public class Day2{
 	public static void main(String[]args){
 		System.out.println("Ryan Polilen");
 		System.out.println("Alexandria Bayas");
+
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(8);
+		list.add(9);
+		list.add(5);
+		list.add(0);
+
+		System.out.println(convertToInt(list));
+
+
+	}
+
+	public static int convertToInt(ArrayList<Integer> list){
+		int j = 1;
+		int c = 0;
+		for(int i = list.size()-1; i >= 0; i--){
+			c+=(list.get(i)*j);
+			j*=10;
+		}
+		return c;
 	}
 }
