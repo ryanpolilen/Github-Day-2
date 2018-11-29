8771b1474ba6c6b16718da30f914bc434752754f
ryanpolilen <45399061+ryanpolilen@users.noreply.github.com>
2018-11-29 12:12:17 -0500

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+
 public class Day2{
 	public static void main(String[]args){
 		System.out.println("Ryan Polilen");
 		System.out.println("Alexandria Bayas");
+
+		System.out.println(convertToArrayList(8950));
 	}
+	public static ArrayList<Integer> convertToArrayList(int i){
+		ArrayList<Integer> list = new ArrayList<>();
+
+		while (i!=0){
+			list.add(0, i%10);
+			i/=10;
+		}
+
+
+
+		return list;
+	}
+
 }
